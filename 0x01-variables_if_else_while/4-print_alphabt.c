51dc9efb38b123a609d914b195466c3592aa952f
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-07 14:26:22 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,16 @@
 
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char a;
-	
-	for (a = 'a'; a <='z'; a++)
-		if (a != 'q' && a !='e')
+
+	for (a = 'a'; a <= 'z'; a++)
+		if (a != 'q' && a != 'e')
 		putchar(a);
 	putchar('\n');
 	return (0);
