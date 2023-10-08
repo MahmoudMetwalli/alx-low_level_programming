88625d10717bfbf021e9d10f2b6f1883128cd50d
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-08 13:22:16 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+
+		b = a + 1;
+		while (b < 10)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 8 && b == 9)
+				break;
+
+			putchar(',');
+			putchar(' ');
+			b++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+		
