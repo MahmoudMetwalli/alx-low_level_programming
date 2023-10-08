8f10f4b47c75dc12df67ca03235816065289c2b0
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-08 13:28:02 -0700

    t

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+
+	int number_1, number_2;
+
+	for (number_1 = 0; number_1 <= 98; number_1++)
+	{
+		number_2 = number_1 + 1;
+		while (number_2 <= 99)
+		{
+			putchar((number_1 / 10) + '0');
+			putchar((number_1 % 10) + '0');
+			putchar(' ');
+			putchar((number_2 / 10) + '0');
+			putchar((number_2 % 10) + '0');
+
+			if (number_1 == 98 && number_2 == 99)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			number_2++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
