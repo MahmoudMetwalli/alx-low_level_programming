c4b18fe0939139c37cc80339928eebce1b412ec5
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 12:10:04 -0700

    lmao

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = a;a <= z; a++)
+		print_alphabet(a);
+	_putchar('\n')
+	return (0);
+}
