eb43a8221833c22ba9d41d2d3045c7bd18a73742
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 12:19:25 -0700

    t

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+;void print_alphabet(void);
 {
 	char a;
 
-	for (a = a;a <= z; a++)
+	for (a = 'a';a <= 'z'; a++)
 		_putchar(a);
-	_putchar('\n')
+	_putchar('\n');
 	return (0);
 }
