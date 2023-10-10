042810046b36b83b14cfe48aac0b3c93bf76a4ba
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 12:28:33 -0700

    push

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	char putChar[] = "_putchar";
-	int num;
-	/* This for loop takes each character/element of putChar[] and print it */
-	for (num = 0; num <= 7; num++)
+	int n;
+	
+	for (n = 0; n <= 7; n++)
 	{
-		putchar(putChar[num]);
+		putchar(putChar[n]);
 	}
-	/* adding a new line after printing "_putchar" */
+	
 	putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
@@ -10,7 +10,9 @@ void print_alphabet(void)
 	char a;
 
 	for (a = 'a';a <= 'z'; a++)
+	{
 		_putchar(a);
+	}
 	_putchar('\n');
 	return (0);
 }
