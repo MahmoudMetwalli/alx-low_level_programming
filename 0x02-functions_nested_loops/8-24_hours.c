32ebdd64294bfef419dc2dd2ce84b15b2adb3d91
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 15:28:10 -0700

    yes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day 
  */
 void jack_bauer(void)
 {
@@ -15,8 +15,8 @@ void jack_bauer(void)
 		for (b = 0; b < 60; b++)
 		{
 			_putchar((a / 10) + '0');
-                	_putchar((a % 10) + '0');
-               		_putchar(':');
+			_putchar((a % 10) + '0');
+			_putchar(':');
 			_putchar((b / 10) + '0');
 			_putchar((b % 10) + '0');
 			_putchar('\n');
