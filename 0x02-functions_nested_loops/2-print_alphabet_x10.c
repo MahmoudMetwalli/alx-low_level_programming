4f0348e35515ba15b811490f9fffab1e59f083bd
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 12:56:43 -0700

    iiiii

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the english alphabet from a-z 10 times
+ * print_alphabet_x10 - prints the english alphabet from a-z 10 times
  * Return: Always 0 (Success)
  */
 
