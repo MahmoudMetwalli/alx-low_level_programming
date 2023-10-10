17b4fe60be8764d245077e450c4b36dac7e0a550
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 12:51:58 -0700

    t push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the english alphabet from a-z 10 times
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char g;
+	int b;
+
+	b = 0
+	while (b <= 9)
+	{
+		for (g = 'a'; g <= 'z'; g++)
+		{
+			_putchar(g)
+		}
+		_putchar('\n')
+		b++
+	}
+}
