9578270304c5c868b0fa9fcd2740ba8933a7f299
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 16:25:47 -0700

    iiii

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,27 +10,31 @@ void times_table(void)
 	int b;
 	int c;
 
-	a = 0;
+	a = 0
 	while (a < 10)
 	{
 		for (b = 0; b < 10; b++)
 		{	
 			c = b * a;
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			if (c >= 10)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				_putchar(',');
+			}
+			else if (c <= 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar(c + '0');
 			}
 			else
 			{
 				_putchar(c + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
-			_putchar(' ');
-			if (b == 9)
-				break;
 		}
 		_putchar('\n');
 		a++;
