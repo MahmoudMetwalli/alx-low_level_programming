89444567a059f16260f887f73b5856a17e28d651
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 18:00:13 -0700

    noo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,13 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (c >= 10)
+			else if (c >= 100)
+			{
+				_putchar((c / 100) + '0');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else if (c >= 10; c <=99)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
