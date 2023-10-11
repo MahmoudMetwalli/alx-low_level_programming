fcacde3b1b5898526ce22575086ce2224aa9a062
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 17:03:11 -0700

    no

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,6 +20,8 @@ void print_to_98(int n)
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 			}
+			_putchar(',');
+			_putchar(' ');
 			n ++;
 		}
 	}
@@ -28,14 +30,28 @@ void print_to_98(int n)
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
 	}
-	else 
+	else if (n >= 98 && n < 100)
 	{
-		while (n >= 98)
+		while (n >= 98 && n < 100)
 		{
 			_putchar((n / 10) + '0');
 			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
 			n --;
 		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			_putchar((n / 100) + '0');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+		n --;
 	}
 	_putchar('\n');
 }
