3b0c6bdc41007a2b979517114a78e94151083fb7
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 17:36:06 -0700

    iiii

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,49 +9,24 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
-		{	
-			if (n < 10 && n > -10)
-			{
-				_putchar(n + '0');
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
+		while (n < 98)
+		{
+			printf("%d, ", n);
 			n ++;
 		}
+		printf(98'\n');
 	}
-	else if (n == 98)
-	{	
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 98 && n < 100)
+	else if (n > 98)
 	{
-		while (n >= 98 && n < 100)
+		while (n > 98)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 			n --;
 		}
+		printf(98'\n');
 	}
 	else
 	{
-		while (n >= 98)
-		{
-			_putchar((n / 100) + '0');
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		_putchar(',');
-		_putchar(' ');
-		n --;
+		printf("98");
 	}
-	_putchar('\n');
 }
