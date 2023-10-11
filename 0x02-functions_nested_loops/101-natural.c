3234682200700c111e0e50c389b75b2b0b93b267
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-10 18:23:41 -0700

    noo

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdioh>
+
+/**
+ * main - Write a function that prints the n times table,
+ * Return: the last number
+ */
+
+int main(void)
+{
+	int theL = 1024;
+	int S = 0;
+	int n;
+
+	for (n = 0; n < L; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			S += n;
+		}
+	}
+	printf("%d\n", S);
+
+	return (0);
+}
