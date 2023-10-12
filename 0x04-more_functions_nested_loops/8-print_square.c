6cf03b244d0fdc6a0dcb9d40439913d56d2a47d3
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-12 15:36:02 -0700

    j

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,22 @@
 void print_square(int size)
 {
 	int i;
+	int j;
 
 	i = 1;
 	while (i <= size)
 	{
-		_putchar('#');
+		j = 1;
+		while (j <= size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 }
