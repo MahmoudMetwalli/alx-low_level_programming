92c43044de3d2f733c31e899cb52c7dc0f5260a9
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-16 15:52:52 -0700

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,3 +5,15 @@
  * puts2 - prints every other character of a string
  * @str: string to be printed
  */
+void puts2(char *str)
+{
+	int b;
+
+	b = 0;
+	while (*(str + b) != '\0')
+	{
+		_putchar(*(str + b));
+		b = b + 2;
+	}
+	_putchar('\n');
+}
