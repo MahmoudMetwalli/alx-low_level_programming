d467b725d569d98953474627b367176bafe9fc80
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-16 15:58:05 -0700

    m

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,18 @@
  */
 void puts2(char *str)
 {
-	int b;
+	int b, a;
 
 	b = 0;
-	while (*(str + b) != '\0')
+	while (*(s + b) != '\0')
+	{
+		b++;
+	}
+	a = 0;
+	while (a < b)
 	{
 		_putchar(*(str + b));
-		b = b + 2;
+		a = a + 2;
 	}
 	_putchar('\n');
 }
