90030b1d344954f7a287d8124941b562d26203a6
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-18 12:18:12 -0700

    m

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a, b, c, d, e;
+	int a, b, d, e;
 
 	b = 0;
-	for (a = 0; *(s1 + a) != '\0'; a++)
+	d = 0;
+	for (a = 0; *(s1 + a) != '\0' || *(s2 + a) != '\0'; a++)
 	{
 		b = b + *(s1 + a);
-	}
-	d = 0;
-	for (c = 0; *(s2 + c) != '\0'; c++)
-	{
-		d = d + *(s2 + c);
+		d = d + *(s2 + a);
 	}
 	e = b - d;
 	return (e);
