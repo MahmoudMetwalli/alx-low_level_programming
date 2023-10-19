9f0909ff54a659b5d73bdc5aee1241b921b2feaa
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-19 10:22:34 -0700

    m

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * *rot13 -  encodes a string using rot13
+ * @a: string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *a)
+{
+	int b;
+
+	for (b = 0; a[b] != '\0'; b++)
+	{
+		if ((a[b] >= 65 && a[b] <= 77) || (a[b] >= 97 && a[b] <= 109))
+		{
+			a[b] = a[b] + 13;
+		}
+		else if ((a[b] >= 78 && a[b] <= 90) || (a[b] >= 110 && a[b] <= 122))
+		{
+			a[b] = a[b] - 13;
+		}
+	}
+	return (a);
+}
