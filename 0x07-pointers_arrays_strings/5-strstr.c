7daf542af2d84e97c415de9ed0878bad1401d138
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-23 15:23:27 -0700

    t pu

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * *_strstr -  locates a substring
+ * @haystack: scanned string
+ * @needle: string to be looked up
+ * Return: pointr or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, k, j, l;
+
+	i = 0;
+	l = 0;
+	while (needle[i] != '\0')
+	{
+		i++;
+	}
+	for (k = 0; haystack[k] != '\0'; k++)
+	{	
+		j = l + 0;
+		if (haystack[k] == needle[j])
+		{
+			l++;
+		}
+		else
+		{
+			l = 0;
+		}
+		if (l == i - 1)
+		{
+			return (haystack + (k - i));
+		}
+	}
+	return (0);
+}	
