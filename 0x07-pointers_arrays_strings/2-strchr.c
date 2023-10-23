edd527d2c1d27e069f2b76ab34f670e3f214f856
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-23 12:23:51 -0700

    m

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,13 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *p;
 
+	i = 0;
 	while ((*(s + i) != c) && (*(s + i) != '\0'))
 	{
 		if (*(s + i) == c)
 		{
-			return (s + i)
+			return (s + i);
 		}	
 		else
 		{
