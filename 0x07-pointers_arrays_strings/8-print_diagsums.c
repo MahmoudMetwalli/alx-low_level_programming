a5ac64c37d587d816b93e47f0516d53f95b4a93d
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-23 18:08:45 -0700

    m

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 	
 	i = 0;
 	j = 0;
-	for (k = 0; k < size; k++}
+	for (k = 0; k < size; k++)
 	{
 		i = (*(a + (k * size + k) * 4)) + i;
 		j = (*(a + ((k * size + ((size - k) * 4))))) + j;
