f28a7df1fceaee28e999d11b5952d99c13da708d
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-27 22:08:09 +0200

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry
+ * @argc: num of arguments
+ * @argv: value of aruments
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+(void)*argv;
+printf("%d\n", argc);
+return (0);
+}
