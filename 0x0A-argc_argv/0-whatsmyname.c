6924618e9df4bc986e99e238213a844cbe146bd9
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-27 21:19:40 +0200

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,6 +9,6 @@
  */
 int main(int argc, char *argv[])
 {
-    printf("%s\n, argv[0]");
-    return (0);
+    printf("%s\n", argv[0]); /*prints program name*/
+    return (0);              /*ends the program*/
 }
