30bae6b3803092a78a3ba2e9c1bc59343b9dc6d6
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-28 00:47:56 +0200

    last

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,28 +11,37 @@
  */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
+	int i;
+	int j;
+	int c;
+	int sum;
+	char *s;
 
-if (argc > 1)
-    {
-        for (i = 1; i < argc; i++)
-        {
-            if (isalpha(*argv[i]) == 0)
-            {
-                if (atoi(argv[i]) < 0)
-                {
-                printf("Error\n");
-                return (1);
-                }
-                sum += atoi(argv[i]);
-            }
-            else
-            {
-            printf("Error\n");
-            return (1);
-            }
-        }
-    }
-printf("%d\n", sum);
-return (0);
+	sum = 0;
+	c = 0;
+
+	i = 1;
+	while (i < argc)
+	{
+		s = argv[i];
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] < '0' || s[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		c = atoi(argv[i]);
+		sum = sum + c;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
