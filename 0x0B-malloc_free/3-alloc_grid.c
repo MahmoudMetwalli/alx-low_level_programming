c43c3bfb899742c697cbf380273ff0bb9f3c5b7d
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-31 01:27:31 +0200

    dynamic allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
 /**
  * **alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: width of the array
- * @hight: height of the array
+ * @height: height of the array
  * Return: pointer to 2 dimensional array of integers or null
  */
 int **alloc_grid(int width, int height)
