7f98154dde3ea1a6d3fa38538e7c18df0484359e
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-30 19:26:44 +0200

    this is about dynamic allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - pointer to allocated space, containing the string given
+ * @str: given string
+ * Return: pointer to new address
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *str2;
+
+	if (str == 0)
+	{
+		return (0);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		str2 = malloc(i * sizeof(char));
+	if (str2 != 0)
+	{
+		for (j = 0; j < i; j++)
+		{
+			str2[j] = str[j];
+		}
+		return (str2);
+	}
+	else
+	{
+		return (0);
+	}
+}
