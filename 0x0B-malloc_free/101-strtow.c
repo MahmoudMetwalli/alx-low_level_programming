1ecf60b243829cc07d9b8c38c0b7a34ca2607731
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-01 00:22:42 +0200

    this is dynamic allocation

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,40 +6,80 @@
  * @str: string to be splitted
  * Return: pointer to arrays or null
  */
+int number(char *str)
+{
+	int a, num = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			num++;
+		}
+	}
+	return (num);
+}
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+void free_everything(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+ */
 char **strtow(char *str)
 {
-	int i = 0, j = 1, k = 0, l, m, r;
-	char **s;
+	int total_words = 0, b = 0, c = 0, length = 0;
+	char **words, *found_word;
 
 	if (str == 0 || *str == 0)
+		return (NULL);
+	total_words = number(str);
+	if (total_words == 0)
+		return (NULL);
+	words = malloc((total_words + 1) * sizeof(char *));
+	if (words == 0)
+		return (NULL);
+	for (; *str != '\0' &&  b < total_words;)
 	{
-		return (0);
-	}
-	while (str[i] != '\0')
-	{
-		if (str[i] == ' ' && str[(i - 1)] != ' ')
+		if (*str == ' ')
+			str++;
+		else
 		{
-			j++;
+			found_word = str;
+			for (; *str != ' ' && *str != '\0';)
+			{
+				length++;
+				str++;
+			}
+			words[b] = malloc((length + 1) * sizeof(char));
+			if (words[b] == 0)
+			{
+				free_everything(words, b);
+				return (NULL);
+			}
+			while (*found_word != ' ' && *found_word != '\0')
+			{
+				words[b][c] = *found_word;
+				found_word++;
+				c++;
+			}
+			words[b][c] = '\0';
+			b++; c = 0; length = 0; str++;
 		}
-		i++;
 	}
-	s = malloc(sizeof(char *) * j);
-	if (s == 0)
-	{
-		return (0);
-	}
-	for (l = 0; l < j; l++)
-	{
-		for (r = 0; s[l][r] != '\0'; r++)
-		s[l] = malloc(sizeof(char) * r);
-		m = 0;
-		while ((str[k] == ' ' && str[(k - 1)] != ' ') || k < i)
-		{
-			s[l][m] = str[k];
-			m++;
-			k++;
-		}
-	}
-	s[l][(m + 1)] = '\0';
-	return (s);
+	return (words);
 }
