3e2c118ecaed479bb1b3e40f32d7cd4e4db9a7fe
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-31 21:12:45 +0200

    this is dynamic allocation

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to be splitted
+ * Return: pointer to arrays or null
+ */
+char **strtow(char *str)
+{
+	int i = 0, j = 1, k = 0, l, m;
+	char **s;
+
+	if (str == 0 || str == "")
+	{
+		return (0);
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == " " && str[(i - 1)] != " ")
+		{
+			j++;
+		}
+		i++;
+	}
+	s = malloc(sizeof(char *) * j);
+	if (s == 0)
+	{
+		return (0);
+	}
+	for (l = 0; l < j; l++)
+	{
+		m = 0;
+		while (str[k] == " " && str[(k - 1)] != " " || k < i)
+		{
+			s[l][m] = str[k];
+			m++;
+			k++;
+		}
+	}
+	s[l][(m + 1)] = '\0';
+	return (s);
+}
