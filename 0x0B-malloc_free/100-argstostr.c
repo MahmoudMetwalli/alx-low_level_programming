f100b9477d2786afbd2b5152bc95b4314ab3b066
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-31 18:03:10 +0200

    this is dynamic allocation

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,7 +9,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i = 0, j, k, c = 0, l = 0;
+	int i = 0, j = 0, k, c = 0, l = 0, m;
 	char *ar;
 
 	if (ac == 0 || av == 0)
@@ -18,7 +18,6 @@ char *argstostr(int ac, char **av)
 	}
 	while (i < ac)
 	{
-		j = 0;
 		while (av[i][j] != '\0')
 		{
 			j++;
@@ -26,28 +25,25 @@ char *argstostr(int ac, char **av)
 		}
 		i++;
 	}
+	c = c + ac + 1;
 	ar = malloc(sizeof(char) * c);
 	if (ar == 0)
 	{
 		return (0);
 	}
-	i = 0;
-	while (i < ac)
+	for (m = 0; m < ac; m++)
 	{
 		j = 0;
-		while (av[i][j] != '\0')
+		while (av[m][j] != '\0')
 		{
 			j++;
 		}
-		k = 0;
-		while (k < j)
+		for (k = 0; k < j; k++)
 		{
-			ar[l] = av[i][k];
-			k++;
+			ar[l] = av[m][k];
 			l++;
 		}
 		ar[l] = '\n';
-		i++;
 	}
 	return (ar);
 }
