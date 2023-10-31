d9d2240f2af6b5750780d162f2c566485b2bf67e
MahmoudMetwalli <mammkbih@gmail.com>
2023-10-31 17:15:32 +0200

    this is dynamic allocation

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: num of arugments
+ * @av: array of strings
+ * Return: pointer to array of arrays or null
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k;
+	char **ar;
+
+	if (ac == 0 || av == 0)
+	{
+		return (0);
+	}
+	ar = malloc(sizeof(char *) * ac);
+	if (ar == 0)
+	{
+		return (0);
+	}
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (ar[i][j] != '\0')
+		{
+			j++;
+		}
+		while (k < j)
+		{
+			ar[i][k] = av[i][k];
+			k++;
+		}
+		k += 1;
+		ar[i][k + 1] = '\n';
+		i++;
+	}
+	return (ar);
+}
