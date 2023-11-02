15e27054615e473618579cdb3479483f56b768f0
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-02 23:44:54 +0200

    more for dyamic allocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: num of elemnts
+ * @size: size of elemnts
+ * Return: pointer or null
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *s;
+	unsigned int i;
+
+	s = malloc(nmemb * size);
+	if (s == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		s[i] = 0;
+	}
+	return (s);
+}
