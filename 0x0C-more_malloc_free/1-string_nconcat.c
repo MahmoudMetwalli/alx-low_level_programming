3a52f6c38f99de272f35f3a5656e5ec903e5c83d
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-02 23:14:55 +0200

    more for dyamic allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ unsigned int strnl(char *s)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l, m = 0, h = 0;
+	unsigned int l, k, m = 0, h = 0;
 	char *p;
 
 	if (s1 == NULL)
@@ -39,10 +39,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 	l = strnl(s1);
+	k = strnl(s2);
+	if (n >= k)
+	{
+		n = k;
+	}
 	p = malloc((sizeof(*p) * (l + n)) + 1);
 	if (p == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	while (m < (l + n) && s1[m] != '\0')
 	{
