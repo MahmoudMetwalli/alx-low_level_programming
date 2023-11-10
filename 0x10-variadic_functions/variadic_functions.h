eec9610effa19fe5cf6da59b0a8184ee13be6a3d
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-10 02:34:22 +0200

    This is variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC
 #define VARIADIC
+#include <stdarg.h>
+/**
+ * struct type - to select type of printing
+ * @h: specifier
+ * @g: function to print
+ */
+typedef struct type
+{
+	char h;
+	void (*g)(va_list);
+} type;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
