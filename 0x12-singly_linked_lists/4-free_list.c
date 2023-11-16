14a8a0abd8b1024e29067f8fa0ad963e3ddab9be
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-17 00:22:55 +0200

    linked lists task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,15 +9,18 @@ void free_list(list_t *head)
 {
 	list_t *crnt;
 
-	while (head && head->next)
+	if (head)
 	{
-		crnt = head;
-		head = head->next;
-		free(crnt->str);
-		free(crnt->next);
-		free(crnt);
+		while (head && head->next)
+		{
+			crnt = head;
+			head = head->next;
+			free(crnt->str);
+			free(crnt->next);
+			free(crnt);
+		}
+		free(head->str);
+		free(head->next);
+		free(head);
 	}
-	free(head->str);
-	free(head->next);
-	free(head);
 }
