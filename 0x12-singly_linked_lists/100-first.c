1f96c8f9511dd038e3305c6f664d8835de258782
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-17 02:08:51 +0200

    linked lists task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * first - to print before main.
+ */
+void first(void)
+{
+	printf("%s\n%s\n", "You're beat! and yet, you must allow,",
+		   "I bore my house upon my back!"
+		   );
+}
