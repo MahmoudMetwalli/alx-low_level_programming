f757dea0365b90c4e15ec5d93f4689b54fb76c52
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-20 22:19:39 +0200

    for a tomorrow linked with the past

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			curr = curr->next;
 			t++;
 		}
-		if (!curr->next)
+		if (!curr->next && t != index)
 		{
 			return (NULL);
 		}
