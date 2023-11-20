6f7f07146888316e7456140e31565ef0f0976c00
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-20 19:56:16 +0200

    for a tomorrow linked with the past

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees a list and set NULL
+ * @head: struct to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+
+	if (*head)
+	{
+		while ((*head)->next)
+		{
+			curr = (*head)->next;
+			free(*head);
+			*head = curr;
+		}
+		free(*head);
+	}
+	*head = NULL;
+}
