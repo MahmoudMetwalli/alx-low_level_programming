58467b8bbe23e106d29818486b0681f35dea9b78
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-28 01:54:32 +0200

    Ready for shell

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,8 +21,6 @@ void copy(int fd_from, int fd_to, char *f_from, char *f_to)
 		dprintf(2, "Error: Can't read from file %s\n", f_from);
 		exit(98);
 	}
-	else if (printed == 0)
-		return;
 	while (printed)
 	{
 		flag = write(fd_to, buff, printed);
@@ -63,7 +61,7 @@ int main(int argc, char **argv)
 	fd_to = open(argv[2], O_WRONLY | O_TRUNC);
 	if (fd_to == -1)
 	{
-		fd_to = open(argv[2], O_WRONLY | O_CREAT, 0664);
+		fd_to = open(argv[2], O_WRONLY | O_CREAT, 0644);
 		if (fd_to == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
