c39758b41413898b8bff869ee313193b9ad16792
MahmoudMetwalli <mammkbih@gmail.com>
2023-11-28 00:37:00 +0200

    what is in is out

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,23 +15,21 @@ int _strlen(char *s)
 	return (b);
 }
 /**
- * create_file - function that creates a file.
+ * append_text_to_file - appends text at the end of a file
  * @filename: filename
  * @text_content: contents
  * Return: 1 on success and -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, flag;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
-		if (fd == -1)
-			return (-1);
+		return (-1);
 	}
 	if (!text_content)
 		return (1);
