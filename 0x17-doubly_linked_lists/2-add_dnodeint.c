9c9671893d5f8c146643ff4a5292d2387d8d3631
MahmoudMetwalli <mammkbih@gmail.com>
2024-01-11 19:25:03 +0200

    2024-01-11 07:25:03 PM

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,11 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	printf("here!");
 	new->n = n;
 	new->next = *head;
-	(*head)->prev = new;
 	new->prev = NULL;
+	if ((*head))
+		(*head)->prev = new;
 	*head = new;
 	return (new);
 }
