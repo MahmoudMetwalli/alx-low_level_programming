442c66cab9b69be0548275f1745868f0f8e19607
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-02 00:50:47 +0200

    2024-02-02 12:50:47 AM

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **Hash_array;
 	hash_table_t *Hash_T;
 
-	Hash_array = malloc(sizeof(hash_node_t) * (size + 1));
+	Hash_array = malloc(sizeof(hash_node_t) * (size));
 	if (!Hash_array)
 		return (NULL);
 	Hash_T = malloc(sizeof(hash_table_t));
