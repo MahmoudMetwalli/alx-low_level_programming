a9df30fd44536bb85a091273aeda84beca21aa7c
MahmoudMetwalli <mammkbih@gmail.com>
2024-02-02 13:37:48 +0200

    2024-02-02 01:37:48 PM

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,10 +10,7 @@ void hash_table_print(const hash_table_t *ht)
 	int flag = 0;
 
 	if (!ht)
-	{
-		printf("{}\n");
 		return;
-	}
 	printf("{");
 	for (compare = 0; compare < ht->size; compare++)
 	{
