d75d9096e6431b5139fb8667727c4a2390190127
MahmoudMetwalli <mammkbih@gmail.com>
2024-03-21 03:04:21 +0200

    2024-03-21 03:04:21 AM

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,13 +8,21 @@ def island_perimeter(grid):
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
+                if j == 0:
+                    if i == 0 or i == (len(grid[i]) - 1) :
+                        perimeter += 2
+                    else:
+                        perimeter += 1
+                if i == 0:
+                    if j != 0:
+                        perimeter += 1
                 try:
                     if grid[i + 1][j] == 0:
                         perimeter += 1
                 except IndexError:
                     perimeter += 1
                 try:
-                    if grid[i - 1][j] == 0:
+                    if grid[i - 1][j] == 0 and (i - 1) >= 0:
                         perimeter += 1
                 except IndexError:
                     perimeter += 1
@@ -24,7 +32,7 @@ def island_perimeter(grid):
                 except IndexError:
                     perimeter += 1
                 try:
-                    if grid[i][j - 1] == 0:
+                    if grid[i][j - 1] == 0 and (j - 1) >= 0:
                         perimeter += 1
                 except IndexError:
                     perimeter += 1
