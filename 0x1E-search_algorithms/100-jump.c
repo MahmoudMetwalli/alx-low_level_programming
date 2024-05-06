c901da88224079ca0eeb51c976ab5752a02a587c
MahmoudMetwalli <mammkbih@gmail.com>
2024-05-06 11:45:06 +0300

    2024-05-06 11:45:06 AM

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,21 +30,9 @@ int jump_search(int *array, size_t size, int value)
 		lower += jump;
 		higher += jump;
 	}
-/**
-*	if (lower >= size)
-*	{
-*		lower -= jump;
-*		higher -= jump;
-*		printf("Value found between indexes [%ld] and [%ld]\n", lower, higher);
-*		if (higher >= size)
-*			higher = size - 1;
-*		for (iterate = lower; iterate <= higher; iterate++)
-*		{
-*			printf("Value checked array[%ld] = [%d]\n", iterate, array[iterate]);
-*			if (array[iterate] == value)
-*				return (iterate);
-*		}
-*	}
-*/
+	lower -= jump;
+	higher -= jump;
+	printf("Value found between indexes [%ld] and [%ld]\n", lower, higher);
+	printf("Value checked array[%ld] = [%d]\n", lower, array[lower]);
 	return (-1);
 }
